9df1c9609dbfc16d7a21af680ff9caee3784a33c
Bernie Boscoe <bernieboscoe@gmail.com>
2018-03-07 11:57:31 -0800

    add table

--- javascripts/client.js
+++ javascripts/client.js
@@ -1,3 +1,5 @@
+
+
 $.ajax({
   url: "https://data.nasa.gov/resource/gymh-eyc2.json",
   data: {},
@@ -8,3 +10,11 @@ $.ajax({
     //todo: handle error gracefully
   }
 });
+/* Note that the whole content variable is just a string */
+var content = "<table>"
+for(i=0; i<3; i++){
+    content += '<tr><td>' + 'result ' +  i + '</td></tr>';
+}
+content += "</table>"
+
+$('#here_table').append(content);
