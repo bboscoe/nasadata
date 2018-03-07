ead157166225866ecd255510cb3307caf4a3d953
Bernie Boscoe <bernieboscoe@gmail.com>
2018-03-07 15:13:28 -0800

    pre tag

--- javascripts/client.js
+++ javascripts/client.js
@@ -4,17 +4,12 @@ $.ajax({
   url: "https://data.nasa.gov/resource/gymh-eyc2.json",
   data: {},
   success: function(data) {
-    $("pre").text(JSON.stringify(data, null, "\t"));
+    for(let i=0;i<data.length;i++){
+      $("pre").append( '<tr><td>' + 'result' +  i + '</td></tr>' );
+      //.text(JSON.stringify(data, null, "\t"));
+    }
   },
   error: function(msg) {
     //todo: handle error gracefully
   }
 });
-/* Note that the whole content variable is just a string */
-var content = "<table>"
-for(i=0; i<3; i++){
-    content += '<tr><td>' + 'result ' +  i + '</td></tr>';
-}
-content += "</table>"
-
-$('#here_table').append(content);
