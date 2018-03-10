5f0d92418f1c119ba0e794fa978d96ce27b89d60
Bernie Boscoe <bernieboscoe@gmail.com>
2018-03-10 13:29:10 -0800

    formatting results

--- javascripts/client.js
+++ javascripts/client.js
@@ -5,7 +5,7 @@ $.ajax({
   data: {},
   success: function(data) {
     for(let i=0;i<data.length;i++){
-      $("pre").append( '<tr><td>' + data[i].category + data[i].sample_material+ '</td></tr>' );
+      $("pre").append( '<tr><td>' + data[i].category +' '+ data[i].sample_material+ '</td></tr>' );
       console.log(data[i]);
       //.text(JSON.stringify(data, null, "\t"));
     }
