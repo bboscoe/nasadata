32087971409e2309a50e5c6f6244dcbe7264b5e8
Bernie Boscoe <bernieboscoe@gmail.com>
2018-03-11 11:24:53 -0700

    next button

--- javascripts/client.js
+++ javascripts/client.js
@@ -15,3 +15,32 @@ $.ajax({
     //https://soda.demo.socrata.com/resource/earthquakes.json?$limit=5&$offset=0
   }
 });
+//Object Helper Functions
+    oFunctions = {};
+    oFunctions.keys = {};
+
+    //NEXT KEY
+    oFunctions.keys.next = function(o, id){
+        var keys = Object.keys( o ),
+            idIndex = keys.indexOf( id ),
+            nextIndex = idIndex += 1;
+        if(nextIndex >= keys.length){
+            //we're at the end, there is no next
+            return;
+        }
+        var nextKey = keys[ nextIndex ]
+        return nextKey;
+    };
+
+    //PREVIOUS KEY
+    oFunctions.keys.previous = function(o, id){
+        var keys = Object.keys( o ),
+            idIndex = keys.indexOf( id ),
+            nextIndex = idIndex -= 1;
+        if(idIndex === 0){
+        //we're at the beginning, there is no previous
+            return;
+        }
+        var nextKey = keys[ nextIndex ]
+        return nextKey;
+    };
