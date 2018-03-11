781ff1671eeaeee2a60aa0a03a42adc4e4cec174
Bernie Boscoe <bernieboscoe@gmail.com>
2018-03-11 15:33:14 -0700

    q

--- javascripts/client.js
+++ javascripts/client.js
@@ -1,14 +1,15 @@
 
 
 $(document).ready(function() {
-$('#foo').click(function () {
-  console.log("in function");
+
+  //console.log("in function");
   // everything happens inside of here, when the button is clicked
   let j=0;
   $.ajax({
     url: "https://data.nasa.gov/resource/gymh-eyc2.json",
     data: {},
     success: function(data) {
+      $('#foo').click(function () {
       for(let i=0;i<data.length;i++){
         $("pre").append( '<tr><td>' + data[i].category +' '+ data[i].sample_material+ '</td></tr>' );
         console.log(data[i]);
