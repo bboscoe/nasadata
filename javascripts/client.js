ed8e2a689553696958f8a24086820fc0703f1dcf
Bernie Boscoe <bernieboscoe@gmail.com>
2018-03-11 13:32:16 -0700

    99

--- javascripts/client.js
+++ javascripts/client.js
@@ -2,18 +2,19 @@
 
 
 $('button').on('click', function () {
-// everything happens inside of here, when the button is clicked
-let j=0;
-$.ajax({
-  url: "https://data.nasa.gov/resource/gymh-eyc2.json?$limit=5&$offset=j",
-  data: {},
-  success: function(data) {
-    for(let i=0;i<data.length;i++){
-      $("pre").append( '<tr><td>' + data[i].category +' '+ data[i].sample_material+ '</td></tr>' );
-      console.log(data[i]);
+  // everything happens inside of here, when the button is clicked
+  let j=0;
+  $.ajax({
+    url: "https://data.nasa.gov/resource/gymh-eyc2.json?$limit=5&$offset=j",
+    data: {},
+    success: function(data) {
+      for(let i=0;i<data.length;i++){
+        $("pre").append( '<tr><td>' + data[i].category +' '+ data[i].sample_material+ '</td></tr>' );
+        console.log(data[i]);
+      }
     };
 
 
-});
+  });
 
 });
